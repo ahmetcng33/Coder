5721db16a3898eaa350b5da02f6c2c92420fbbe4
ahmetcng33 <ahmetcingi333@gmail.com>
2022-10-12 15:01:22 +0300

    .

---
+++ React/Session-2/rc02-props/rc02_props/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Msg from './companents/Msg';
+
+function App() {
+  return (
+    <div>
+      <Msg name= "Felix"/>
+    </div>
+  );
+};
+
+export default App;
---
+++ React/Session-2/rc02-props/rc02_props/src/companents/Msg.jsx
@@ -0,0 +1,11 @@
+const Msg = ({name}) => {
+
+//  const { name } = props ;
+ return(
+        <div>
+            <h3>Merhaba {name}</h3>
+        </div>
+    );
+};
+
+export default Msg;
